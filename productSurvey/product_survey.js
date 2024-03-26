2a80aeb57666c895aa111b88c1542e00ec77dd4f
gego-00 <ggzambrna@gmail.com>
2024-03-25 20:26:48 -0400

    extra-input

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -1,4 +1,5 @@
 function submitFeedback() {
+    
     const username = document.getElementById('name').value;
     const age = document.getElementById('age').value;
     const email = document.getElementById('email').value;
@@ -6,11 +7,10 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
-    const submitButton=document.getElementById('submitBtn');
-
-    submitButton.onclick = submitFeedback;
-    alert('Thank you for your valuable feedback')
-
+    const experience = document.getElementById('experienceText').value;
+    
+    document.getElementById('userInfo').style.display = 'block';
+    
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
     document.getElementById('userEmail').innerHTML = email;
@@ -18,12 +18,19 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
-    document.getElementById('userInfo').style.display = 'block';
+    document.getElementById('userExperience').innerHTML = experience;
     
-    document.addEventListener('keydown', function(event) {
-        if (event.key === 'Enter') {
-          submitFeedback();
-        }
-    });
-
+    console.log(username,age,feedback);
+    alert('Thank you for your valuable feedback')
+    
 }
+
+
+document.getElementById('submitBtn').onclick = submitFeedback;
+
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        submitFeedback();
+    }
+});
